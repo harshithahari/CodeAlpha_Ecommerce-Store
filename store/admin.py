8cac8d50ec8008a05ce6102d6dcc89492faaa423
Harshitha Hari <234568829+harshithahari@users.noreply.github.com>
2026-06-25 05:20:27 +0530

    Rename store to Online Store

--- store/admin.py
+++ store/admin.py
@@ -4,3 +4,6 @@ from .models import Product, Order, OrderItem
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.site_header = "Online Store Admin"
+admin.site.site_title = "Online Store Admin Portal"
+admin.site.index_title = "Welcome to Online Store Management Portal"
